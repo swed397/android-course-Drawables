ad629a8f6b9432d33c25892a40d4a48543f9e0f5
Rodion <swed197@mail.ru>
2023-08-17 01:12:06 +0500

    Created all view without logic

---
+++ app/src/main/java/com/android/course/tic_tac_toe_game/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.course.tic_tac_toe_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
